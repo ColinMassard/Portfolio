6ca88022401505ff2ce20b4d29658156324b3adb
ColinMassard <colinmassard@gmail.com>
2023-08-05 00:52:14 +0200

    UP(Metadata)

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
   useHead({
     title: 'Colin Massard | Portfolio',
     meta: [
-      { name: 'description', content: 'My amazing site.' }
+      { name: 'description', content: 'I am a passionate and versatile front-end developer with expertise in cutting-edge web technologies. With a strong background in UX/UI design, I create captivating and intuitive UI. Beyond development, my fascination with horology drives me to blend precision and aesthetics in my creations.' }
     ],
     bodyAttrs: {
       class: 'home'
